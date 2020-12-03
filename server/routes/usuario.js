5109e7ae89890c4a8f8f12369101e0df09983d28
bryan.jara <jara_bryan97@outlook.com>
2020-12-03 02:05:07 -0600

    Se creo el modelo de categoria y se creo el get y post del mismo

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -54,16 +54,6 @@ const app = express();
       });
     });
   
-  app.delete('/usuario/:id', function(req, res){
-      let id = req.params.id;
-  
-      res.json({
-        ok: 200,
-        mensaje: 'Usuario eliminado con exito',
-        id: id
-      });
-  });
-  
   app.put('/usuario/:id', function (req, res){
       let id = req.params.id;
       let body = _.pick(req.body, ['nombre', 'email']);
@@ -85,5 +75,42 @@ const app = express();
           });
       });
   });
+
+  app.delete('/usuario/:id', function(req, res){
+    /*let id = req.params.id;
+
+    Usuario.deleteOne({ _id: id }, (err, usuarioBorrado) => {
+      if(err) {
+        return res.status(400).json({
+          ok: true,
+          msg: 'Ocurrio un error al momento de eliminar',
+          err
+        });
+      }
+      res.json({
+        ok: true,
+        msg: 'Usuario eliminado con exito',
+        usuarioBorrado
+      });
+    });*/
+
+    let id = req.params.id;
+    
+    Usuario.findByIdAndUpdate(id, { estado: false }, 
+      { new: true, runValidators: true, context: 'query' }, (err, usrDB) => {
+        if(err) {
+          return res.status(400).json({
+            ok: true,
+            msg: 'Ocurrio un error al momento de eliminar',
+            err
+          });
+        }
+        res.json({
+          ok: true,
+          msg: 'Usuario eliminado con exito',
+          usrDB
+        });
+      });
+});
 
   module.exports = app;
